5f911a1bd1098f8768ad7f0a5aadb2cb580445a8
Prashanth Chintala <pchintala16@gmail.com>
2020-06-29 11:51:59 -0700

    Completed Largest Val in row using BFS

---
+++ BFSLargestValueRow.java
@@ -0,0 +1,62 @@
+// Time Complexity : O(n). n is the number of nodes in a tree
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        //base case
+        if(root==null) return new ArrayList();
+        
+        int max=Integer.MIN_VALUE;
+        List<Integer>list = new ArrayList();
+        
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        list.add(root.val);
+        
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            max=Integer.MIN_VALUE;
+            for(int i=0;i<size;i++){
+                TreeNode node = q.poll();
+                if(node.left!=null){
+                    q.add(node.left);
+                    if(max<node.left.val){
+                        max=node.left.val;
+                    }
+                }
+                
+                if(node.right!=null){
+                    q.add(node.right);
+                    if(max<node.right.val){
+                        max=node.right.val;
+                    }
+                }
+                
+            }
+            list.add(max);
+        }
+        list.remove(list.size()-1);
+        return list;
+    }
+}
