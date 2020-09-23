5da81c75f7e4d827beb80dc4d2bc71c92be452b5
asureban1 <54301735+asureban1@users.noreply.github.com>
2020-09-23 15:47:39 -0500

    Update Find LargestValueinEachTreeRow.java

--- Find LargestValueinEachTreeRow.java
+++ Find LargestValueinEachTreeRow.java
@@ -24,18 +24,26 @@ class Solution {
         
         if(root == null)    return new ArrayList();
         
-        List<Integer> output = new ArrayList();
+        List<Integer> output = new ArrayList(); 
+      
+        //to store TreeNodes from each level of the given tree
         Deque<TreeNode> queue = new LinkedList();
         queue.offer(root);
+      
+        //initialize the max to minimum value of integer so as to obtain the valid 
+        //maximum value in corner cases; for e.g. if the nodes have negative numbers
         int max = Integer.MIN_VALUE;
         
         while(!queue.isEmpty()){
+            // size of the queue at each level
             int size = queue.size();
             max = Integer.MIN_VALUE;
             
+          //traverse the queue and add it's left and right children if present
             for(int i = 0; i < size;i++){
                 TreeNode node = queue.poll();
                 
+                //calculate maximum value which will be separate for each level
                 max = Math.max(max, node.val);
                 if(node.left != null){
                     queue.offer(node.left);
@@ -45,6 +53,7 @@ class Solution {
                 }
                 
             }
+            //add maximum value to the output arraylist
             output.add(max);
         }
         return output;
