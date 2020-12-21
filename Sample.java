29ab0ba0013c31147e2f1f39aa8070d2428d6af4
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-21 10:10:02 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,28 @@
 
 
 // Your code here along with comments explaining your approach
+
+  public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        helper(root, res, 0);
+        return res;
+    }
+    private void helper(TreeNode root, List<Integer> res, int d){
+        if(root == null){
+            return;
+        }
+       //expand list size
+        if(d == res.size()){
+            res.add(root.val);
+        }
+        else{
+        //or set value
+            res.set(d, Math.max(res.get(d), root.val));
+        }
+        helper(root.left, res, d+1);
+        helper(root.right, res, d+1);
+    }
+
+
+// TC  : O (N)
+// SC : O(N)
