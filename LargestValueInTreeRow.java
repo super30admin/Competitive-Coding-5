fdcfa95b66515c1365fb824294be82ad06d7bdb1
deepashree <dkulkarni2@scu.edu>
2021-01-27 23:10:55 -0800

    Added DFS solution

--- LargestValueInTreeRow.java
+++ LargestValueInTreeRow.java
@@ -27,3 +27,31 @@ class Solution {
         return result;
     }
 }
+
+//DFS
+//TC: O(N)
+//SC: O(Height)
+class Solution {
+    List<Integer> result;
+    public List<Integer> largestValues(TreeNode root) {
+      result = new ArrayList();
+        if ( root == null)
+         return result;
+        dfs(root, 0);
+        return result;
+    }
+    private void dfs(TreeNode root, int level)
+    {
+        //base
+        if ( root == null)
+            return;
+        //logic
+        if (level == result.size())
+            result.add(root.val);
+        else
+            result.set(level, Math.max(result.get(level), root.val));
+        dfs(root.left, level+1);
+        dfs(root.right, level+1);
+        
+    }
+}
