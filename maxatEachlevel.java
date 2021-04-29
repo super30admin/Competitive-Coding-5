7dcc3756449ebf373ce88d55d08108f2418a4925
Yash Shah <yash2396@hotmail.com>
2021-04-29 11:30:25 -0700

    Completed Sprint Competitive-Coding-5

---
+++ maxatEachlevel.java
@@ -0,0 +1,40 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            for(int i = 0 ; i < size ; i++){
+                TreeNode curr = q.poll();
+                max = Math.max(max,curr.val);
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right!= null) q.add(curr.right);
+            }
+            result.add(max);
+        }
+        return result;
+    }
+}
