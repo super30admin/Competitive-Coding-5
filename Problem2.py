1363e2271962079e7256a74bf3b476e6795eeeb1
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-06-28 18:32:40 +0530

    indentation

--- Problem2.py
+++ Problem2.py
@@ -5,38 +5,38 @@
 	# Any problem you faced while coding this : No
 
     class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        count = [0]*9
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] != ".":
-                    a = int(board[i][j])
-                    count[a-1] += 1
-                    if count[a-1] > 1:
-                        return False
+        def isValidSudoku(self, board: List[List[str]]) -> bool:
             count = [0]*9
-        count = [0]*9
-        print("1")
-        for j in range(len(board[0])):
+            
             for i in range(len(board)):
-                if board[i][j] != ".":
-                    a = int(board[i][j])
-                    count[a-1] += 1
-                    if count[a-1] > 1:
-                        return False
-            count = [0]*9
-        count = [0]*9
-        print("2")
-        for i in range(0,len(board),3):
-            for j in range(0,len(board[0]),3):
-                for k in range(i,i+3,1):
-                    for l in range(j,j+3,1):
-                        if board[k][l] != ".":
-                            a = int(board[k][l])
-                            count[a-1] += 1
-                            if count[a-1] > 1:
-                                return False
+                for j in range(len(board[0])):
+                    if board[i][j] != ".":
+                        a = int(board[i][j])
+                        count[a-1] += 1
+                        if count[a-1] > 1:
+                            return False
                 count = [0]*9
-        return True
-                
+            count = [0]*9
+            print("1")
+            for j in range(len(board[0])):
+                for i in range(len(board)):
+                    if board[i][j] != ".":
+                        a = int(board[i][j])
+                        count[a-1] += 1
+                        if count[a-1] > 1:
+                            return False
+                count = [0]*9
+            count = [0]*9
+            print("2")
+            for i in range(0,len(board),3):
+                for j in range(0,len(board[0]),3):
+                    for k in range(i,i+3,1):
+                        for l in range(j,j+3,1):
+                            if board[k][l] != ".":
+                                a = int(board[k][l])
+                                count[a-1] += 1
+                                if count[a-1] > 1:
+                                    return False
+                    count = [0]*9
+            return True
+                    
