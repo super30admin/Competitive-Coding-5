04562d57af50d35cd2e600c74b0f66dd7b882e43
Aniruddha Ingle <aniruddhaingle@Aniruddhas-MacBook-Pro.local>
2022-01-25 07:59:12 -0600

    Competitive Coding by Ani

---
+++ MaxValAtLevel.py
@@ -0,0 +1,32 @@
+# Time Complexity : O(N)
+# Space Complexity : O(H)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+# Your code here along with comments explaining your approach
+
+# Traverse level by level. Use size variable to keep track of max within a level.
+from collections import deque
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        q = deque([ ])
+        q.append(root)
+        result = []
+        while q:
+            size = len(q)
+            maxVal = float("-inf")
+            for _ in range(size):
+                curr = q.popleft()
+                maxVal = max(maxVal, curr.val)
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+                    
+            result.append(maxVal)
+        
+        return result
