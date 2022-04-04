bb061a42fbf37947a567e8b7ebbaaf7cb442f6af
architakale <architakale@knights.ucf.edu>
2022-04-04 03:23:43 -0400

    Competitive Code-5

---
+++ suduko.py
@@ -0,0 +1,43 @@
+# Time Complexity : O(N^2)
+# Space Complexity : O(N^2)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        N = 9
+
+        # Use an array to record the status
+        rows = [[0] * N for _ in range(N)]
+        cols = [[0] * N for _ in range(N)]
+        boxes = [[0] * N for _ in range(N)]
+
+        for r in range(N):
+            for c in range(N):
+                # Check if the position is filled with number
+                if board[r][c] == ".":
+                    continue
+
+                pos = int(board[r][c]) - 1
+
+                # Check the row
+                if rows[r][pos] == 1:
+                    return False
+                rows[r][pos] = 1
+
+                # Check the column
+                if cols[c][pos] == 1:
+                    return False
+                cols[c][pos] = 1
+                
+                 # Check the box
+                idx = (r // 3) * 3 + c // 3
+                if boxes[idx][pos] == 1:
+                    return False
+                boxes[idx][pos] = 1
+
+        return True
