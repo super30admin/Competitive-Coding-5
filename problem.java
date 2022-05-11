11c4e393e7cf2704a40dcd133c5af8c4ec2a1fa4
priyank <ppatel631@gmail.com>
2022-05-11 19:49:53 -0400

    Completed sudoku problem

---
+++ problem.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+class validSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        
+        HashSet<String> seen = new HashSet<>();
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                char curr = board[i][j];
+                if(curr != '.'){
+                    if(!seen.add(curr + "found in row" + i) ||
+                        !seen.add(curr + "found in col" + j) ||
+                        !seen.add(curr + "found in sub boxes" + i/3 + "-" + j/3))
+                       
+                       return false;
+                }
+            }
+        }
+         return true;              
+    }
+}
+
+//time complexity O(1)
+//space complexity O(n) where n is number of row/col
