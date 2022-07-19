7b5227c2be0d4b8469a10c7a753e9c11e4679800
tathyapatel <92098849+tathyapatel@users.noreply.github.com>
2022-07-18 19:29:54 -0500

    Create FindlargestValueEachTreeLevel.py

---
+++ FindlargestValueEachTreeLevel.py
@@ -0,0 +1,31 @@
+#515. Find Largest Value in Each Tree Row
+"""
+Time Complexity : O(n)
+Spae Complexity : O(n)
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return None
+        q = deque()
+        q.append(root)
+        result = []
+        while q:
+            size = len(q)
+            maximum = float('-inf')
+            for i in range(size):
+                x = q.popleft()
+                maximum = max(maximum, x.val)
+                if x.left:
+                    q.append(x.left)
+                if x.right:
+                     q.append(x.right)
+            result.append(maximum)
+
+        return result
