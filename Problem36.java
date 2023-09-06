8635f594f6d10207e8a8b9db3de28f1c1cba16c7
chsrimanaswi <53294091+chsrimanaswi@users.noreply.github.com>
2023-09-05 23:08:04 -0400

    Update Problem36.java

--- Problem36.java
+++ Problem36.java
@@ -1,5 +1,5 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity : O(1)-> O(81)
+// Space Complexity : O(1)-> O(81)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
  
