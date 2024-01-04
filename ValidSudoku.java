db0ca79a39c89fbe5a6cd31073012167a72beafa
Siri-isha <150191332+Siri-isha@users.noreply.github.com>
2024-01-03 22:56:12 -0600

    Update TC ValidSudoku.java

--- ValidSudoku.java
+++ ValidSudoku.java
@@ -1,32 +1,58 @@
-// Time Complexity : O(m*n)
+// Time Complexity : O(9*9*(9+9+9))
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this :
 
 
 // For each value in input matrix, perform row level, column level and box level validations
-public class ValidSudoku{
-    public boolean validSudoku(char[][] board){
+class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
         for(int i=0;i<board.length;i++){
-            for(int j=0; j<board[0].length; j++){
-                if(board[i][j]!='.')
-                    return isValid(board,i,j);
+            for(int j=0; j<board[0].length;j++){
+                if(board[i][j]!='.'){
+                    if(!isValidHelper(board,i,j))
+                        return false;
+                }
             }
         }
-        return  true;
+        return true;
     }
 
-    private boolean isValid(char[][] board, int i, int j){
+    private boolean isValidHelper(char[][] board,int i, int j){
         char val = board[i][j];
 
-        //check row
+        //check for row
         for(int k=0;k<9;k++){
-            if(board[k][j]==val && k!=i)
-                return false;
+            if(k!=j){
+                if(board[i][k]==val)
+                    return false;
+            }
         }
-        //check col
 
-        //check box
+        //check for column
+        for(int l=0;l<9;l++){
+            if(l!=i){
+                if(board[l][j]==val)
+                    return false;
+            }
+        }
+
+        //check for current box
+        int boxRow = i;
+        int boxCol = j;
+        while(boxRow%3>0)
+            boxRow--;
+        while(boxCol%3>0)
+            boxCol--;
+        for(int m=boxRow;m<boxRow+3;m++){
+            for(int n=boxCol;n<boxCol+3;n++){
+                if(m==i && n==j){}
+                else{
+                    if(board[m][n]==val)
+                        return false;
+                }
+            }
+        }
         return true;
     }
 }
